195250033c488ef209813978ebdb43a5d93b42af
Mokshda Gangrade <mokshdagangrade@gmail.com>
2022-06-02 23:01:25 +0530

    Added navigation

---
+++ app/src/main/java/com/example/mugs/ChartFragment.kt
@@ -0,0 +1,117 @@
+package com.example.mugs
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+
+class ChartFragment : Fragment() {
+
+    private lateinit var lineChart: LineChart
+    private var scoreList = ArrayList<Score>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_chart, container, false)
+
+        lineChart = view.findViewById(R.id.chart1)
+
+
+        initLineChart()
+
+
+        setDataToLineChart()
+
+        return view
+    }
+    private fun initLineChart() {
+
+//        hide grid lines
+        lineChart.axisLeft.setDrawGridLines(false)
+        val xAxis: XAxis = lineChart.xAxis
+        xAxis.setDrawGridLines(false)
+        xAxis.setDrawAxisLine(false)
+
+        //remove right y-axis
+        lineChart.axisRight.isEnabled = false
+
+        //remove legend
+        lineChart.legend.isEnabled = false
+
+
+        //remove description label
+        lineChart.description.isEnabled = false
+
+
+        //add animation
+        lineChart.animateX(1000, Easing.EaseInSine)
+
+        // to draw label on xAxis
+        xAxis.position = XAxis.XAxisPosition.BOTTOM_INSIDE
+        xAxis.valueFormatter = MyAxisFormatter()
+        xAxis.setDrawLabels(true)
+        xAxis.granularity = 1f
+        xAxis.labelRotationAngle = +90f
+
+    }
+
+
+    inner class MyAxisFormatter : IndexAxisValueFormatter() {
+
+        override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+            val index = value.toInt()
+            return if (index < scoreList.size) {
+                scoreList[index].name
+            } else {
+                ""
+            }
+        }
+    }
+
+    private fun setDataToLineChart() {
+        //now draw bar chart with dynamic data
+//        val entries: ArrayList<Entry> = ArrayList()
+//
+//        scoreList = getScoreList()
+//
+//        //you can replace this data object with  your custom object
+//        for (i in scoreList.indices) {
+//            val score = scoreList[i]
+//            entries.add(Entry(i.toFloat(), score.score.toFloat()))
+//        }
+//
+//        val lineDataSet = LineDataSet(entries, "")
+//
+//        val data = LineData(lineDataSet)
+//        lineChart.data = data
+//
+//        lineChart.invalidate()
+    }
+
+    // simulate api call
+    // we are initialising it directly
+    private fun getScoreList(): ArrayList<Score> {
+        scoreList.add(Score("John", 56))
+        scoreList.add(Score("Rey", 75))
+        scoreList.add(Score("Steve", 85))
+        scoreList.add(Score("Kevin", 45))
+        scoreList.add(Score("Jeff", 63))
+
+        return scoreList
+    }
+}
--- app/src/main/java/com/example/mugs/MainActivity.kt
+++ app/src/main/java/com/example/mugs/MainActivity.kt
@@ -3,11 +3,18 @@ package com.example.mugs
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.mugs.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = FirebaseAuth.getInstance()
@@ -17,7 +24,21 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_chart, R.id.navigation_pie
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
     }
 }
